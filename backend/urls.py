fad3c3fba8903f0d7b8e40bd8eb498737bbeba4e
Таалайбек <taalaibek@192.168.1.8>
2024-11-17 18:53:52 +0600

    CURL page added

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,9 @@ from django.urls import include, path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -39,4 +42,6 @@ urlpatterns = [
     path('api/', include('products.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("dashboard/", TemplateView.as_view(template_name="dashboard.html"), name="dashboard"),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
